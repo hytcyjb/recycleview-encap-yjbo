53093d6bc0b65953db404ab88af836be47dd4939
yjbo <1457521527@qq.com>
2017-04-01 21:39:22 +0800

    调试了gridview添加分割线成功了；是通过引入LineGridView绘画进行处理的

--- app/src/main/java/com/yy/yjbo/recycleview_encap_yjbo/test/gridview/main/GirdViewActivity.java
+++ app/src/main/java/com/yy/yjbo/recycleview_encap_yjbo/test/gridview/main/GirdViewActivity.java
@@ -11,7 +11,8 @@ import com.yy.yjbo.recycleview_encap_yjbo.test.list.main.SimpleAdapter;
 import java.util.ArrayList;
 
 /**
- * 参考：http://www.cniao5.com/forum/thread/2ac69d820f0611e790dc00163e0230fa
+ * 网格参考：http://www.cniao5.com/forum/thread/2ac69d820f0611e790dc00163e0230fa
+ * 分割线参考：http://www.2cto.com/kf/201602/489367.html
  * 对GridView标签进行封装
  * @author yjbo
  * @time 2017/4/1 21:11
