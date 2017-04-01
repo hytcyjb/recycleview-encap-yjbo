c9e28ad82a5f5044a1f614fad05f6a383212a930
yjbo <1457521527@qq.com>
2017-04-01 21:07:55 +0800

    对listview的标签进行的封装

--- app/src/main/java/com/yy/yjbo/recycleview_encap_yjbo/test/list/main/AdapterActivity.java
+++ app/src/main/java/com/yy/yjbo/recycleview_encap_yjbo/test/list/main/AdapterActivity.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 /**
  * 参考：http://www.cniao5.com/forum/thread/2ac69d820f0611e790dc00163e0230fa
- * 对listview的标签进行的封装
+ * 对listview标签进行封装
  * @author yjbo
  * @time 2017/4/1 21:05
  */
