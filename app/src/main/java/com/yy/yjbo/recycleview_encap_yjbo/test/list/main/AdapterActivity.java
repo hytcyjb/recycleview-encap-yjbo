acc5d84d37964741ff61ed67c54912bb17bf377e
yjbo <1457521527@qq.com>
2017-04-01 21:25:18 +0800

    调试了listview添加分割线成功了

--- app/src/main/java/com/yy/yjbo/recycleview_encap_yjbo/test/list/main/AdapterActivity.java
+++ app/src/main/java/com/yy/yjbo/recycleview_encap_yjbo/test/list/main/AdapterActivity.java
@@ -24,7 +24,7 @@ public class AdapterActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list);
         initView();
         initData();
     }
