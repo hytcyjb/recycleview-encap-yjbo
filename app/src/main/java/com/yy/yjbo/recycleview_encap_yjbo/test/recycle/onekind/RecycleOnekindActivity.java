bd4741603931c6d54930436b938db6941a3526e8
yjbo <1457521527@qq.com>
2017-04-01 23:11:55 +0800

    recycleview的单种类型的界面

---
+++ app/src/main/java/com/yy/yjbo/recycleview_encap_yjbo/test/recycle/onekind/RecycleOnekindActivity.java
@@ -0,0 +1,59 @@
+package com.yy.yjbo.recycleview_encap_yjbo.test.recycle.onekind;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.yy.yjbo.recycleview_encap_yjbo.R;
+import com.yy.yjbo.recycleview_encap_yjbo.test.util.Item;
+
+import java.util.ArrayList;
+
+/**
+ * 参考：http://www.cniao5.com/forum/thread/107b6b5210fe11e790dc00163e0230fa
+ * recycleview的单种类型的界面
+ *
+ * @author yjbo
+ * @time 2017/4/1 23:10
+ */
+
+public class RecycleOnekindActivity extends Activity {
+    //    private SimpleAdapter mAdapter;
+    private ArrayList<Item> Datas;
+    private RecyclerAdapterDemo mAdapterDemo;
+    private RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycle_onekind);
+        initView();
+        initData();
+    }
+
+    protected void initView() {
+        mRecyclerView = (RecyclerView) findViewById(R.id.swipe_target_onekind);
+        //添加分割线
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+//        //添加布局管理器--列表
+//        mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        //添加布局管理器--网格
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 4));
+    }
+
+    //添加数据
+    protected void initData() {
+        Datas = new ArrayList<>();
+        for (int i = 1; i <= 30; i++) {
+            Item item = new Item(R.mipmap.ic_launcher_round, " get 新技能" + i, "拣到漂亮妹子 " + i + " 枚，在大街上");
+            Datas.add(item);
+        }
+        mAdapterDemo = new RecyclerAdapterDemo(this, Datas);
+        mRecyclerView.setAdapter(mAdapterDemo);
+    }
+
+
+}
---
+++ app/src/main/java/com/yy/yjbo/recycleview_encap_yjbo/test/recycle/onekind/RecyclerAdapterDemo.java
@@ -0,0 +1,49 @@
+package com.yy.yjbo.recycleview_encap_yjbo.test.recycle.onekind;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.yy.yjbo.recycleview_encap_yjbo.R;
+import com.yy.yjbo.recycleview_encap_yjbo.test.util.Item;
+
+import java.util.List;
+
+public class RecyclerAdapterDemo extends RecyclerViewAdapter<Item> {
+
+    public RecyclerAdapterDemo(Context context, List<Item> datas) {
+        super(context, R.layout.girdview_item, datas);
+    }
+
+    @Override
+    protected void bindData(RecyclerViewHolder holder, final Item item, int position) {
+
+        holder.setText(R.id.tv1, item.getTv1())
+                .setOnClickListener(R.id.tv1, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(mContext, item.getTv1(), Toast.LENGTH_SHORT).show();
+                    }
+                }).setOnLongClickListener(R.id.tv1, new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(mContext, "长按：" + item.getTv1(), Toast.LENGTH_SHORT).show();
+                return true;//true 表示不响应点击事件 false 长按事件结束还会响应点击事件
+            }
+        });
+
+        //加载网络图片
+        holder.setImagePath(R.id.img, new RecyclerViewHolder.ImageLoder("http://tomcat.apache.org/images/tomcat.png") {
+            @Override
+            public void loadImage(ImageView imageView, String path) {
+                Glide.with(mContext)
+                        .load(path)
+                        .placeholder(R.mipmap.ic_launcher_round) // 同样也可以是drawble
+                        .error(R.mipmap.ic_launcher)// 当不能加载时载入
+                        .into(imageView);
+            }
+        });
+    }
+}
